90719b7cf4635a35f720373f60c002520bc3908f
g1pd78 <g1pd78@gmail.com>
2021-03-13 01:11:23 -1000

    now it send flags and wait reply from user.

---
+++ main.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+import requests
+from bs4 import BeautifulSoup as BS #for parsing/ dunno 4wat i use it here
+
+import discord #for my bot
+from discord.ext import commands
+
+import json #load countries indexes
+import requests #requests whatever
+import shutil #2save pics localy
+from random import randint #4random
+
+TOKEN = ''
+
+response = requests.get("https://flagcdn.com/en/codes.json")
+countries = json.loads(response.text)
+indexs = list(countries.keys())
+countries_count = len(indexs)
+
+def load_Image(url):
+
+	response = requests.get(url, stream=True)
+	with open('img.png', 'wb') as out_file:
+		shutil.copyfileobj(response.raw, out_file)
+	del response
+
+
+
+
+
+
+
+game_status = False
+answer = ""
+
+
+bot = commands.Bot(command_prefix='/')
+
+@bot.command(pass_context=True)
+async def play(ctx):
+
+	index = indexs[randint(0, countries_count - 1)]
+	global answer
+	global game_status 
+	game_status = True
+	answer = countries[index]
+	load_Image('https://flagcdn.com/w640/' + index + '.png')
+	await ctx.send(file=discord.File('img.png'))
+
+
+
+@bot.event
+async def on_message(message):
+	if message.author.id == bot.user.id: #he shouldn reply himself
+		return
+	print(bot)
+	print(message.author)
+	global game_status #make it global 2 make it usable in async
+	if game_status:
+		if message.content == answer:
+			await message.channel.send("Yup")
+			game_status = False
+		else:
+			await message.channel.send("Nope")
+
+
+	await bot.process_commands(message)
+
+
+	
+bot.run(TOKEN)
+
+
