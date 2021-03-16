df220d51b9a52f7e3faa59f810fbd1ec03bca117
g1pd78 <g1pd78@gmail.com>
2021-03-16 11:48:05 -1000

    rewrite everything into class add wait_for

--- main.py
+++ main.py
@@ -9,21 +9,16 @@ import json #load countries indexes
 import requests #requests whatever
 import shutil #2save pics localy
 from random import randint #4random
-
+import asyncio
 from os import remove#load just what i need
-
 import sqlite3 #4sqlite
 
-TOKEN = ''
+TOKEN = 'ODIxMzM2OTcyODIzODg3ODcz.YFCPqg.mq1BfL4pGYhJC3NsO4pBYnsgPSA'
 
 response = requests.get("https://flagcdn.com/en/codes.json")
 countries = json.loads(response.text)
 indexs = list(countries.keys())
 countries_count = len(indexs)
-game_status = False
-count = 0
-
-
 
 def load_Image(url):
 
@@ -35,55 +30,6 @@ def load_Image(url):
 def remove_Image():
 	remove('img.png')
 
-
-
-
-
-
-bot = commands.Bot(command_prefix = '/', help_command = None)
-
-@bot.command(pass_context = True)
-async def help(ctx, *args):
-	st = discord.Embed(title = 'Можем сыграть',
-	 description = 
-	 '''
-	 	Присутствуют следующие режимчики:
-
-	 	1)квиз на 5 вопросиков - short
-	 	2)квиз на 10 вопросиков - long
-	 	3)квиз на желаемое число вопросов - custom
-	 	Запускаемся, значится, командой - /play %вариант игры%
-
-	 	На вопросик дается 5 попыточек
-	 ''',
-	 color = 0xFF5733)
-	await ctx.send(embed=st)
-
-@bot.command(pass_context=True)
-async def play(ctx, prm, *args):
-
-	global game_status 
-	global count
-	#param = int(prm)
-
-	#print(args)
-	###################### 4 testing made it 1
-
-	if prm == 'short':
-		count = 1
-		game_status = True
-	elif prm == 'long':
-		count == 10
-		game_status = True
-	elif prm == 'custom':
-		count = int(args[0])
-		game_status = True
-	else:
-		await ctx.send('You shoulda read /help info first!')
-
-
-
-
 #make class for easier usage 
 class image(object):
 
@@ -94,118 +40,223 @@ class image(object):
 		self.count_of_wrong_answers = count_of_wrong_answers
 
 im = image('a', 'a', True, 0)
+
+show_variants = False
+game_status = False
 players_list = {}
 
 
 
-@bot.event
-async def on_message(message):
+
+
+
+
+class myBot(discord.Client):
+	async def on_ready(self):
+		print('im ready!')
+	
 
 	
-	if message.author.id == bot.user.id: #he shouldn reply himself
-		return
-
-	global game_status #make it global 2 make it usable in async
-	global count
-	global players_list
-
-	if message.content == '/play' and game_status:#/play control
-		await message.channel.send('Nah, you are already playing!')
-		return
-
-	if message.content == '/help' and game_status:#/help control
-		await message.channel.send('If you figured how to start - you can finish a game first, just try to answer!')
-		return
-
-	await bot.process_commands(message)#after exeptions
 
 
-	def game():#refreshing info
-		im.index = indexs[randint(0, countries_count - 1)]
-		im.answer = countries[im.index]
-		load_Image('https://flagcdn.com/w640/' + im.index + '.png')
-		im.is_answered = False
-		im.count_of_wrong_answers = 0
-		
+	async def on_message(self, message):
+		if message.author.id == self.user.id: #he shouldn reply himself
+			return
 
-		print(im.answer) ###############################################
+		#here i can place my commands / shoulda check em with /stratswith()
+		ctx = message.content
 
-
-	'''
-	print(bot)
-	print(message.author)
-	print(im.answer)
-	print(game_status)
-	print(count)
-	print(im.is_answered)
-	'''
+		global game_status
+		global players_list
 
 
-	if game_status:
+		'''
+		#dunno shoulda i use it if i check this in guess part
+		if ctx.startswith('/help') or ctx.startswith('/play') and game_status:
+			print('chicha')
+			return'''
 
-		if not im.is_answered:
-			if message.content == im.answer:
-				message_author = str(message.author)
+		if message.content.startswith('/help') and not game_status:
+			st = discord.Embed(title = 'Можем сыграть',
+	 		description = 
+	 		'''
+	 		Присутствуют следующие режимчики:
 
-				if message_author in players_list:
-					players_list[message_author] += 1
-				else:
-					players_list.update({message_author:1})
+	 		1)квиз на 5 вопросиков - short - Default
+	 		2)квиз на 10 вопросиков - long
+	 		3)квиз на желаемое число вопросов - custom %желаемое количество вопросов%
+	 		Запускаемся, значится, командой - /play 
+
+	 		!Включить варианты - /variants ON 
+	 		!Выключить варианты - /variants OFF
+	 		!Default = False
+
+	 		>За правильный ответ в игре без вариантов дается 3 балла
+	 		>За правильный ответ в игре с вариантами дается 1 балл
+	 		>За неправильный ответ в игре с вариантами забирается 2 балла
 
 
-				await message.channel.send("Yup, " + message_author.split('#')[0] + ', excelent!')
-				im.is_answered = True
-				count-=1
+	 		Обкашлять вопросик дается 5 попыточек и 10 секунд
+	 		''',
+	 		color = 0xFF5733)
+			await message.channel.send(embed=st)
+
+
+
+
+
+		if message.content.startswith('/play') and not game_status:
+
+			
+			count = 0
+			if len(message.content.split(' ')) > 1:
+				param = message.content.split(' ')[1]
+
+				if param == 'short':
+					count = 5
+				elif param == 'long':
+					count = 10
+				elif param == 'custom':
+					if len(message.content.split(' ')) > 2:
+						param_custom = message.content.split(' ')[1]
+						if param_custom.isdigit():
+							count = int(param)
+						else:
+							print('Ne chislo')
+					else:
+						print('gde parametr')
 			else:
-				await message.channel.send("Nope")
-				im.count_of_wrong_answers += 1
-				if im.count_of_wrong_answers == 5: #attempts border
-					im.is_answered = True
-					count -= 1
-					await message.channel.send("Too many attempts for one question!")
+				count = 5
+			
+			game_status = True
 
-		if im.is_answered and count:
-			game()#reload image information
-			await message.channel.send(file=discord.File('img.png'))
-			remove_Image()
+			def game():#refreshing info
+				im.index = indexs[randint(0, countries_count - 1)]
+				im.answer = countries[im.index]
+				load_Image('https://flagcdn.com/w640/' + im.index + '.png')
+				im.is_answered = False
+				im.count_of_wrong_answers = 4
 
-		if not count:
+				print(im.answer) ###############################################
+				
+			def print_variants():#rename
+				variant_list = []
+				variant_list.append(im.answer)
+				variants_count = 1
+				while variants_count != 4:
+					current_variant = countries[indexs[randint(0, countries_count - 1)]]
+					if current_variant not in variant_list:
+						variant_list.append(current_variant)
+						variants_count += 1
+				
+				return variant_list
+
+			message_author = str(message.author)
+			points_per_question = 1 if show_variants else 3#points depends by variants
+
+
+			while count:
+				#reaction = await self.wait_for('reaction')
+				#print(reaction)
+
+				game()#reload image information
+				await message.channel.send(file=discord.File('img.png'))
+				remove_Image()
+
+				if show_variants:
+					variant_list = print_variants()
+					for i in range(4):
+						print_cur_var = discord.Embed(color = discord.Colour.random(), description = variant_list[i])
+						await message.channel.send(embed = print_cur_var)
+
+				while im.count_of_wrong_answers:
+
+
+					try:
+						guess = await self.wait_for('message', check = lambda message: message.author.id != self.user.id, timeout = 10.0)
+						#чтобы не отвечал на вэйтфоры сам себе
+
+
+						if guess.content.startswith('/help') or guess.content.startswith('/play'):
+							await message.channel.send('Sorry, your shoulda finnish game at first!')
+						else:
+
+
+
+							print(guess)
+							if guess and guess.content == im.answer:
+								await message.channel.send('Yup')
+								im.count_of_wrong_answers = 0#to end game
+								if message_author in players_list:
+									players_list[message_author] += points_per_question
+								else:
+									players_list.update({message_author:points_per_question})
+
+							else:
+								await message.channel.send('Nope')
+								im.count_of_wrong_answers -= 1
+								if im.count_of_wrong_answers == 0:
+									await message.channel.send('Sorry, your attempts over! It was {}.'.format(im.answer))
+
+
+
+							
+
+
+
+
+					except asyncio.TimeoutError:
+						im.count_of_wrong_answers = 0
+						await message.channel.send('Sorry, you took too long it was {}.'.format(im.answer))
+
+
+
+					
+
+				count -= 1
+
+
 			players_list = dict(sorted(players_list.items(), key = lambda item: item[1], reverse = False))#sort by points
 			await message.channel.send("Game Over!")
 
 			players_list_keys = list(players_list.keys())
 			players_list_values = list(players_list.values())
+			'''
+				if players_list_values[0] == players_list_values[1]:#check it with someone else or create another account //later
+					await message.channel.send("It isnt the END! It is a tie! One more flag!")
+					count += 1 
+					game_status = True
+					return
+				'''
 
-			if players_list_values[0] == players_list_values[1]:#check it with someone else or create another account //later
-				await message.channel.send("It isnt the END! It is a tie! One more flag!")
-				count += 1 
-				game_status = True
-
-			winner_mes = discord.Embed(color = 0xFF5733, description = players_list_keys[0].split('#')[0] + ' is a winner!')
-			await message.channel.send(embed = winner_mes)#print a winner
+			#print(players_list_values)
+			if players_list_keys:
+				winner_mes = discord.Embed(color = 0xFF5733, description = players_list_keys[0].split('#')[0] + ' is a winner!')
+				await message.channel.send(embed = winner_mes)#print a winner
 			#write result into sql dbase
-			connect = sqlite3.connect('players.sql')
-			cur = connect.cursor()
-			print(players_list_values[0])
-			cur.execute("INSERT INTO players(id, score) values(:id, :score);", (players_list_keys + players_list_values))
-			connect.commit()
+				connect = sqlite3.connect('players.sql')
+				cur = connect.cursor()
+				#print(players_list_values[0])
+				cur.execute("INSERT INTO players(id, score) values(:id, :score);", (players_list_keys + players_list_values))
+				connect.commit()
 
 			#i shoulda review tie variants ----- still player who isnt a potential winnner can answer!!!!!!!!!
-			players_list.clear()#remove players from dict
-		
+				players_list.clear()#remove players from dict
 
 
-	
+
+			game_status = False
+
+'''
+	async def on_reaction_add(client, mes, author):
+		print(author.id)
+		#print(author.name)
+		#print(author)
+		#print(m.emoji)
+		'''
 
 
-	
+		#!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+bot = myBot()
 bot.run(TOKEN)
-
-
-#add exeptions like count < 0 and add sql or json table with players 
-#create own table 4every chat or create prm with id of the chat
-#insert into tbl1 values('hello!',10);
-
-
-
-#lambda functions
