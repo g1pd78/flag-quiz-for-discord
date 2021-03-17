ce02b8363584917feae820392121a5426e19a77f
g1pd78 <g1pd78@gmail.com>
2021-03-17 12:08:19 -1000

    transfer /play code into call_play

--- call_play_command.py
+++ call_play_command.py
@@ -5,11 +5,26 @@ from random import randint #4random
 import shutil #2save pics localy
 from os import remove#load just what i need
 import discord
+import asyncio
+from  database_module import *
+
+
 
 response = requests.get("https://flagcdn.com/en/codes.json")
 countries = json.loads(response.text)
 indexs = list(countries.keys())
 countries_count = len(indexs)
+
+
+class image(object):
+
+	def __init__(self, index, answer, count_of_wrong_answers):
+		self.index = index
+		self.answer = answer
+		self.count_of_wrong_answers = count_of_wrong_answers
+
+im = image('', '', 0)
+
 
 
 def load_Image(url):
@@ -51,7 +66,7 @@ def count_calculation(msg: list) -> int:
 
 	return count
 
-async def refresh_and_send_image(im, message):#refreshing info
+async def refresh_and_send_image(message):#refreshing info
 	im.index = indexs[randint(0, countries_count - 1)]
 	im.answer = countries[im.index]
 	load_Image('https://flagcdn.com/w640/' + im.index + '.png')
@@ -63,9 +78,9 @@ async def refresh_and_send_image(im, message):#refreshing info
 
 	return await message.channel.send(file=discord.File('img.png')), remove_Image()
 	
-async def print_variants(message, answer):#rename
+async def print_variants(message):#rename
 	variant_list = []
-	variant_list.append(answer)
+	variant_list.append(im.answer)
 	variants_count = 1
 	while variants_count != 4:
 		current_variant = countries[indexs[randint(0, countries_count - 1)]]
@@ -79,7 +94,55 @@ async def print_variants(message, answer):#rename
 
 
 
+async def play_command(message, show_variants, client):
+	count = count_calculation(message.content.split(' '))
+	game_status = True
+	while count:
+		#reaction = await self.wait_for('reaction')
+		#print(reaction)
+		await refresh_and_send_image(message)#reload image information and send it/ then remove
 
+
+		if show_variants:
+			await print_variants(message)
+
+
+		while im.count_of_wrong_answers:
+
+
+			try:
+				guess = await client.wait_for('message', check = lambda message: message.author.id != client.user.id, timeout = 10.0)
+				#чтобы не отвечал на вэйтфоры сам себе
+
+
+				if guess.content.startswith('/help') or guess.content.startswith('/play'):
+					await message.channel.send('Sorry, your shoulda finnish game at first!')
+				else:
+
+
+
+					#print(guess)
+					if guess and guess.content == im.answer:
+						await message.channel.send('Yup')
+						im.count_of_wrong_answers = 0#to end game
+						point_calculation(str(message.author), show_variants)
+					else:
+						await message.channel.send('Nope')
+						im.count_of_wrong_answers -= 1
+						if im.count_of_wrong_answers == 0:
+							await message.channel.send('Sorry, your attempts over! It was {}.'.format(im.answer))
+
+
+
+					
+
+
+
+
+			except asyncio.TimeoutError:
+				im.count_of_wrong_answers = 0
+				await message.channel.send('Sorry, you took too long it was {}.'.format(im.answer))
+		count -= 1
 
 
 print('play module ready!')
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 from call_help_command import help_command
 from call_play_command import *
-from database_module import *
+
 
 import discord #for my bot
-import asyncio
+
 
 
 TOKEN = ''
@@ -12,14 +12,8 @@ TOKEN = ''
 
 
 #make class for easier Image usage 
-class image(object):
 
-	def __init__(self, index, answer, count_of_wrong_answers):
-		self.index = index
-		self.answer = answer
-		self.count_of_wrong_answers = count_of_wrong_answers
 
-im = image('', '', 0)
 
 show_variants = True
 game_status = False
@@ -33,6 +27,7 @@ game_status = False
 
 class myBot(discord.Client):
 	
+	#def __init__(self, show_variants, game_status):
 
 
 	async def on_ready(self):
@@ -62,74 +57,15 @@ class myBot(discord.Client):
 
 		if message.content.startswith('/play') and not game_status:
 
-			
-			count = count_calculation(message.content.split(' '))
-			game_status = True
+			await play_command(message, show_variants, self)
 
-
-
-
-
-			while count:
-				#reaction = await self.wait_for('reaction')
-				#print(reaction)
-
-				await refresh_and_send_image(im, message)#reload image information and send it/ then remove
-
-
-				if show_variants:
-					await print_variants(message, im.answer)
-
-
-				while im.count_of_wrong_answers:
-
-
-					try:
-						guess = await self.wait_for('message', check = lambda message: message.author.id != self.user.id, timeout = 10.0)
-						#чтобы не отвечал на вэйтфоры сам себе
-
-
-						if guess.content.startswith('/help') or guess.content.startswith('/play'):
-							await message.channel.send('Sorry, your shoulda finnish game at first!')
-						else:
-
-
-
-							#print(guess)
-							if guess and guess.content == im.answer:
-								await message.channel.send('Yup')
-								im.count_of_wrong_answers = 0#to end game
-								point_calculation(str(message.author), show_variants)
-							else:
-								await message.channel.send('Nope')
-								im.count_of_wrong_answers -= 1
-								if im.count_of_wrong_answers == 0:
-									await message.channel.send('Sorry, your attempts over! It was {}.'.format(im.answer))
-
-
-
-							
-
-
-
-
-					except asyncio.TimeoutError:
-						im.count_of_wrong_answers = 0
-						await message.channel.send('Sorry, you took too long it was {}.'.format(im.answer))
-
-
-
-					
-
-				count -= 1
-
-				#need just keys here
-
-			
 			await message.channel.send("Game Over!")
+						await game_finnished(message)
+			game_status = False
 
 			
 			'''
+			#tie variant
 				if players_list_values[0] == players_list_values[1]:#check it with someone else or create another account //later
 					await message.channel.send("It isnt the END! It is a tie! One more flag!")
 					count += 1 
@@ -138,9 +74,8 @@ class myBot(discord.Client):
 				'''
 
 			#print(players_list_values)
-			
-			await game_finnished(message)
-			game_status = False
+
+
 
 '''
 	async def on_reaction_add(client, mes, author):
