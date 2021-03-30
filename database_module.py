498f231b915ac9b0ff1a54d64baccac06b20683d
g1pd78 <g1pd78@gmail.com>
2021-03-30 05:26:32 -1000

    create guild_id param in sqldb

--- database_module.py
+++ database_module.py
@@ -8,7 +8,20 @@ from discord import Embed as em
 players_list = {}
 
 
+#create a table
+def create_db():
+	connect = sqlite3.connect('players.sql')
+	cur = connect.cursor()
+	try:
+		cur.execute("CREATE TABLE IF NOT EXISTS players(guild_id INTEGER , score INTEGER, name TEXT);")
+		connect.commit()
+	except sqlite3.Error as error:
+		print("Ошибка при работе с SQLite", error)
+	connect.close()
 			
+create_db()
+
+
 
 
 def point_calculation(message_author, show_variants, direction):
@@ -36,6 +49,7 @@ async def tie_checker(message) -> bool:
 
 async def game_finnished(message) -> bool:
 	global players_list
+	guild = message.guild.id
 	players_list = dict(sorted(players_list.items(), key = lambda item: item[1], reverse = True))#sort by points
 	players_list_keys = list(players_list.keys())
 
@@ -46,15 +60,21 @@ async def game_finnished(message) -> bool:
 		#write result into sql dbase
 		connect = sqlite3.connect('players.sql')
 		cur = connect.cursor()
+		
 		#\print(players_list_values[0])
 		for i in players_list_keys:
-			check_player = cur.execute("SELECT score FROM players WHERE id = ?;", [i]).fetchone()
-			#print(type(check_player[0]))
+			check_player = cur.execute("SELECT score FROM players WHERE guild_id = ? AND name = ?;", [guild, i]).fetchone()
+			print(type(guild), i)
+			#take score from sql db
 			if check_player:
-				cur.execute("UPDATE players set score = ? WHERE id = ?;", [players_list[i] + check_player[0], i])
+				cur.execute("UPDATE players set score = ? WHERE name = ? AND guild_id = ?;", [players_list[i] + check_player[0], i, guild])
+				
+				print('asdsda')
+				#if data exists in table - update
 				#проверить с двузначными, но вроде должно работать 
 			else:
-				cur.execute("INSERT INTO players values(:id, :score);", [i, players_list[i]])
+				cur.execute("INSERT INTO players values(:guild_id, :score, :name);", [guild, players_list[i], i])
+				#if data doesnt exists in table - insert
 
 		connect.commit()
 		players_list.clear()#remove players from dict
@@ -77,7 +97,7 @@ print('database module ready!')
 async def show_score(message):
 	connect = sqlite3.connect('players.sql')
 	cur = connect.cursor()
-	check_player = cur.execute("SELECT score FROM players WHERE id = ?;", [str(message.author)]).fetchone()
+	check_player = cur.execute("SELECT score FROM players WHERE name = ? AND guild_id = ?;", [str(message.author), message.guild.id]).fetchone()
 	await message.channel.send(check_player[0])
 	connect.close()
 
